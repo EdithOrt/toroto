25349b8999b5fdb07aaaddd3baa461e3ad61b259
JimenaMV <jimenamanzanares.v@gmail.com>
2020-09-04 16:22:35 -0500

    Bubble chart default

--- src/components/GraphicBubble/GraphicBubble.jsx
+++ src/components/GraphicBubble/GraphicBubble.jsx
@@ -1,7 +1,10 @@
 import React from "react";
 import Highcharts from "highcharts";
 import HighchartsReact from "highcharts-react-official";
-//import packedbubble from "highcharts/modules/packedbubble";
+import optionsBubble from "../../utils/optionsBubble";
+/* import "highcharts/highcharts-more"; */
+import HC_more from "highcharts/highcharts-more";
+HC_more(Highcharts);
 
 /* const options = {
   series: [
@@ -12,20 +15,10 @@ import HighchartsReact from "highcharts-react-official";
   ],
 }; */
 
-const options = {
-  series: [
-    {
-      name: "profit",
-      data: [100, 200, 30, 100, 30, 58, 100],
-    },
-  ],
-};
-
 const GraphicBubble = () => {
   return (
     <div>
-      <HighchartsReact highcharts={Highcharts} options={options} />
-      {/* <HighchartsReact highcharts={packedbubble} options={options} /> */}
+      <HighchartsReact highcharts={Highcharts} options={optionsBubble} />
     </div>
   );
 };
