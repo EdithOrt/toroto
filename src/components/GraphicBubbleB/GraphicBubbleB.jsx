83342778a6a47c9bb688f655d129800be18996eb
JimenaMV <jimenamanzanares.v@gmail.com>
2020-09-05 17:43:07 -0500

    Adding Data.json

--- src/components/GraphicBubbleB/GraphicBubbleB.jsx
+++ src/components/GraphicBubbleB/GraphicBubbleB.jsx
@@ -2,11 +2,22 @@ import React from "react";
 import Highcharts from "highcharts";
 import HighchartsReact from "highcharts-react-official";
 import optionsBubbleB from "../../utils/optionsBubbleB";
-/* import "highcharts/highcharts-more"; */
 import more from "highcharts/highcharts-more";
 more(Highcharts);
 
+
+
 const GraphicBubbleB = () => {
+  
+/* 
+  useEffect(() => {
+    fetch("https://api.example.com/items")
+      .then(response => {
+        console.log(response)
+      })
+  }, []) */
+
+
   return (
     <div>
       <HighchartsReact highcharts={Highcharts} options={optionsBubbleB} />
