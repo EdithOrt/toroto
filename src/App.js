6f4054e40aa907b1575a81092daa439943c3afb9
EdithOrt <eoc.diseno@gmail.com>
2020-09-06 19:17:16 -0500

    Section login styles and functionallity

--- src/App.js
+++ src/App.js
@@ -1,17 +1,28 @@
 import React from "react";
-import FirebaseData from "./components/FirebaseData/FirebaseData";
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
 import GraphicBubble from "./components/GraphicBubble/GraphicBubble";
 import GraphicBars from "./components/GraphicBars/GraphicBars";
 import GraphicAreaspline from "./components/GraphicAreaspline/GraphicAreaspline"
+import LogIn from "./components/LogIn/LogIn";
 
 function App() {
+
+
   return (
     <div>
-      Toroto
-      <FirebaseData />
-      <GraphicBubble />
-      <GraphicBars />
-      <GraphicAreaspline />
+      <Router>
+        <Switch>
+          <Route path="/mi-huella">
+            <GraphicBars />
+            <GraphicAreaspline />
+            <GraphicBubble />
+          </Route>
+          <Route path="/">
+            <LogIn />
+          </Route>
+        </Switch>
+      </Router>
+
     </div>
   );
 }
